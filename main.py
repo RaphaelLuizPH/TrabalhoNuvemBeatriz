5e3c5450bf3a575afdcdd6f1dc6f6be56b60edf9
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2024-04-30 11:14:17 -0300

    Changes to be committed: 	modified:   main.py 	modified:   static/css/main.css 	modified:   templates/index.html 	new file:   templates/login.html

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
-def hello():
-
+def home():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
